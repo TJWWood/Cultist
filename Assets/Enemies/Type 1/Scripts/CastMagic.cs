0c8590040d0af557ab684ba6af2001f8cb976729
Thomas Wood <40295704@live.napier.ac.uk>
2021-01-25 18:20:05 +0000

    Enemy AI and Animations
    Enemy AI for partrolling, chasing and attacking added - alongside character model and animations for walking and attacking.
    Baked NavMesh for the world for AI Navigation.

---
+++ Assets/Enemies/Type 1/Scripts/CastMagic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastMagic : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetTrigger("CastMagic");
+        Debug.Log("Attacking player");
+    }
+}
---
+++ Assets/Scripts/Enemies/Movement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform player;
+    public float sightRange;
+    public float attackRange;
+    public LayerMask playerMask;
+    public Transform[] points;
+    private int destPoint = 0;
+
+    bool playerInSightRange;
+    bool playerInAttackRange;
+
+    bool hasCastMagic;
+    public float castTimer;
+    public float castTime;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        anim = gameObject.GetComponent<Animator>();
+        castTimer = castTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasCastMagic)
+        {
+            castTimer -= Time.deltaTime;
+            if (castTimer <= 0)
+            {
+                hasCastMagic = false;
+                castTimer = castTime;
+            }
+        }
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerMask);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerMask);
+
+        if (!playerInSightRange)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                PatrolPoints();
+            }
+        }
+
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            FollowPlayer();
+        }
+
+        if(playerInAttackRange)
+        {
+            if (!hasCastMagic)
+            {
+                AttackPlayer();
+            }
+        }
+    }
+
+    void PatrolPoints()
+    {
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void FollowPlayer()
+    {
+        agent.destination = player.position;
+    }
+
+    void AttackPlayer()
+    {
+        if(TryGetComponent<CastMagic>(out CastMagic cm))
+        {
+            Destroy(cm);
+            gameObject.AddComponent<CastMagic>();
+        }
+        else
+        {
+            gameObject.AddComponent<CastMagic>();
+        }
+        hasCastMagic = true; 
+    }
+
+    
+    /**
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, sightRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, attackRange);
+    }
+    */
+}
